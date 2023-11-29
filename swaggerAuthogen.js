e9ce9c1becc82e95d924fa1589f37fc6ec0c2281
abdullahinalcik <130457799+abdullahinalcik@users.noreply.github.com>
2023-11-29 11:02:40 +0300

    add:swagger

---
+++ swaggerAuthogen.js
@@ -0,0 +1,51 @@
+"use strict";
+/* -------------------------------------------------------
+    NODEJS EXPRESS | FullStack 
+------------------------------------------------------- */
+
+const swaggerAutogen=require("swagger-autogen")()
+const packageJson = require('./package.json')
+require('dotenv').config()
+
+
+const document = {
+
+
+	info: {
+		version: packageJson.version,
+		title: packageJson.title,
+		description: packageJson.description,
+		termsOfService: "http://www.clarusway.com",
+		contact: { name: packageJson.author, email: "a.inalcik@gmail.com" },
+		license: { name: packageJson.license, },
+	},
+	host: `${process.env.HOST}:${process.env.PORT}`,
+	basePath: '/',
+	schemes: ['http', 'https'],
+	// Token Settings:
+	securityDefinitions: {
+		Token: {
+			type: 'apiKey',
+			in: 'header',
+			name: 'Authorization',
+			description: 'Entry Your SimpleToken for Login. Example: <b>Token <i>...tokenKey...<i></b>'
+		}
+	},
+	security: [{ "Token": true }],
+	definition: {
+        "User":require("./src/models/user").schema.obj ,
+        "Blog":require("./src/models/blog").schema.obj ,
+        "Category":require("./src/models/category").schema.obj ,
+        "Comment":require("./src/models/comment").schema.obj ,
+        "Like":require("./src/models/like").schema.obj ,
+        "View":require("./src/models/view").schema.obj ,
+
+	
+	}
+};
+
+const routes=["./index.js"]
+
+const outputFile="./src/configs/swagger.json"
+
+swaggerAutogen(outputFile,routes,document)
